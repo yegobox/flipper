fe73a320821ed03b4257a07d509c4cc1e344f098
Muragijimana <beastar457@gmail.com>
2025-02-24 22:05:01 +0200

    pipelines (#387)

---
+++ apps/flipper/ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Install CocoaPods using Homebrew.
+brew install cocoapods
+
+# Install Flutter
+brew install --cask flutter
+
+# Run Flutter doctor
+flutter doctor
+
+# Enable macos
+
+# Get packages
+flutter packages get
+
+
+# Build ios app
+flutter build ios --no-codesign
