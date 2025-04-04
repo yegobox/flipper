b9a5bfbd51127d203f08cbbad31eeb06d482f673
Muragijimana <info@yegobox.com>
2025-04-04 12:39:59 +0200

    major fix during call

--- packages/flipper_models/lib/rw_tax.dart
+++ packages/flipper_models/lib/rw_tax.dart
@@ -697,9 +697,10 @@ class RWTax with NetworkHelper, TransactionMixinOld implements TaxApi {
     /// so need to account them in future
     final totalTax = ((taxTotals['B'] ?? 0.0) * 18 / 118);
     talker.warning("HARD COPY TOTALTAX: ${totalTax.toStringAsFixed(2)}");
-
+    // TODO: this name was business.name but was returning wrong name
+    final name = "Kigali Manufacturing Company";
     final topMessage =
-        "${business?.name}\n${ProxyService.box.getUserPhone()!.replaceAll("+", "")}\n${business?.adrs?.isNotEmpty == true ? business?.adrs : 'Kigali, Rwanda'}\n${business?.tinNumber ?? '999909695'}";
+        "${name}\n${ProxyService.box.getUserPhone()!.replaceAll("+", "")}\n${business?.adrs?.isNotEmpty == true ? business?.adrs : 'Kigali, Rwanda'}\n${business?.tinNumber ?? '999909695'}";
 
     talker.error("TopMessage: $topMessage");
     talker.error("TINN: ${business?.tinNumber}");
