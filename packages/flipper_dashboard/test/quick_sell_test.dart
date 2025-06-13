bb0406ad5799ff98710216ceb4ac209e5463d1cf
Muragijimana <info@yegobox.com>
2025-06-13 18:09:45 +0200

    restore tests

--- packages/flipper_dashboard/test/quick_sell_test.dart
+++ packages/flipper_dashboard/test/quick_sell_test.dart
@@ -1,10 +1,10 @@
-// import 'package:flipper_dashboard/QuickSellingView.dart';
-// import 'package:flipper_rw/dependencyInitializer.dart';
-// import 'package:flutter/material.dart';
+import 'package:flipper_dashboard/QuickSellingView.dart';
+import 'package:flipper_rw/dependencyInitializer.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-// import 'TestApp.dart';
+import 'TestApp.dart';
 
 // flutter test test/quick_sell_test.dart  --dart-define=FLUTTER_TEST_ENV=true
 
@@ -14,44 +14,44 @@ class MockRef extends Mock implements WidgetRef {}
 
 void main() {
   group('QuickSellingView Tests', () {
-    // late GlobalKey<FormState> formKey;
-    // late TextEditingController discountController;
-    // late TextEditingController deliveryNoteCotroller;
-    // late TextEditingController customerNameController;
-    // late TextEditingController receivedAmountController;
-    // late TextEditingController customerPhoneNumberController;
-    // late TextEditingController paymentTypeController;
+    late GlobalKey<FormState> formKey;
+    late TextEditingController discountController;
+    late TextEditingController deliveryNoteCotroller;
+    late TextEditingController customerNameController;
+    late TextEditingController receivedAmountController;
+    late TextEditingController customerPhoneNumberController;
+    late TextEditingController paymentTypeController;
 
     setUpAll(() async {
-      // await initializeDependenciesForTest();
+      await initializeDependenciesForTest();
     });
 
     setUp(() {
-      // formKey = GlobalKey<FormState>();
-      // discountController = TextEditingController();
-      // receivedAmountController = TextEditingController();
-      // customerNameController = TextEditingController();
-      // customerPhoneNumberController = TextEditingController();
-      // paymentTypeController = TextEditingController();
-      // deliveryNoteCotroller = TextEditingController();
+      formKey = GlobalKey<FormState>();
+      discountController = TextEditingController();
+      receivedAmountController = TextEditingController();
+      customerNameController = TextEditingController();
+      customerPhoneNumberController = TextEditingController();
+      paymentTypeController = TextEditingController();
+      deliveryNoteCotroller = TextEditingController();
     });
 
     testWidgets('QuickSellingView displays correctly',
         (WidgetTester tester) async {
-      // await tester.pumpWidget(
-      //   TestApp(
-      //     child: QuickSellingView(
-      //       deliveryNoteCotroller: deliveryNoteCotroller,
-      //       formKey: formKey,
-      //       customerNameController: customerNameController,
-      //       discountController: discountController,
-      //       receivedAmountController: receivedAmountController,
-      //       customerPhoneNumberController: customerPhoneNumberController,
-      //       paymentTypeController: paymentTypeController,
-      //     ),
-      //   ),
-      // );
-      // await tester.pumpAndSettle();
+      await tester.pumpWidget(
+        TestApp(
+          child: QuickSellingView(
+            deliveryNoteCotroller: deliveryNoteCotroller,
+            formKey: formKey,
+            customerNameController: customerNameController,
+            discountController: discountController,
+            receivedAmountController: receivedAmountController,
+            customerPhoneNumberController: customerPhoneNumberController,
+            paymentTypeController: paymentTypeController,
+          ),
+        ),
+      );
+      await tester.pumpAndSettle();
 
       // Ensure that the initial values of the text fields are shown
       // expect(find.text('Discount'), findsOneWidget);
@@ -63,23 +63,23 @@ void main() {
 
     testWidgets('QuickSellingView validates form fields',
         (WidgetTester tester) async {
-      // await tester.pumpWidget(
-      //   TestApp(
-      //     child: QuickSellingView(
-      //       deliveryNoteCotroller: deliveryNoteCotroller,
-      //       formKey: formKey,
-      //       customerNameController: customerNameController,
-      //       discountController: discountController,
-      //       receivedAmountController: receivedAmountController,
-      //       customerPhoneNumberController: customerPhoneNumberController,
-      //       paymentTypeController: paymentTypeController,
-      //     ),
-      //   ),
-      // );
+      await tester.pumpWidget(
+        TestApp(
+          child: QuickSellingView(
+            deliveryNoteCotroller: deliveryNoteCotroller,
+            formKey: formKey,
+            customerNameController: customerNameController,
+            discountController: discountController,
+            receivedAmountController: receivedAmountController,
+            customerPhoneNumberController: customerPhoneNumberController,
+            paymentTypeController: paymentTypeController,
+          ),
+        ),
+      );
 
-      // // Trigger form validation
-      // formKey.currentState!.validate();
-      // await tester.pumpAndSettle();
+      // Trigger form validation
+      formKey.currentState!.validate();
+      await tester.pumpAndSettle();
 
       // Verify error messages for invalid inputs
       // expect(find.text('Please enter received amount'), findsOneWidget);
