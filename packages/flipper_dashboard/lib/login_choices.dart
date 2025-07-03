b174e4ef926ee9e196b14ed94f7dee71559afd91
Muragijimana <info@yegobox.com>
2025-07-04 00:10:54 +0200

    debug

--- packages/flipper_dashboard/lib/login_choices.dart
+++ packages/flipper_dashboard/lib/login_choices.dart
@@ -258,6 +258,10 @@ class _LoginChoicesState extends ConsumerState<LoginChoices>
         await startupViewModel.hasActiveSubscription();
 
         final userId = ProxyService.box.getUserId();
+          talker.debug('LoginChoices: userId: $userId');
+          final businessId = ProxyService.box.getBusinessId();
+          talker.debug('LoginChoices: businessId: $businessId');
+
         if (userId != null) {
           final currentShift = await ProxyService.strategy.getCurrentShift(userId: userId);
           if (currentShift == null) {
