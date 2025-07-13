7b8a1afd8a7a338707c20640eaefff09c02d2868
Muragijimana <info@yegobox.com>
2025-07-13 12:32:58 +0200

    feat: improve the drawer and reactivity when switching branch on mobile

--- packages/flipper_dashboard/lib/ProfileFutureWidget.dart
+++ packages/flipper_dashboard/lib/ProfileFutureWidget.dart
@@ -1,33 +1,46 @@
 import 'package:flipper_dashboard/profile.dart';
-import 'package:flipper_models/db_model_export.dart';
-import 'package:flipper_services/proxy.dart';
+import 'package:flipper_models/providers/active_branch_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ProfileFutureWidget extends StatelessWidget {
   const ProfileFutureWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Branch>(
-      future: ProxyService.strategy.activeBranch(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting ||
-            !snapshot.hasData) {
-          return const SizedBox.shrink();
-        }
-        final data = snapshot.data;
-        return Padding(
-          padding: const EdgeInsets.only(right: 12.0),
-          child: SizedBox(
-            height: 40,
-            width: 40,
-            child: ProfileWidget(
-              branch: data!,
-              sessionActive: true,
-              size: 25,
-              showIcon: false,
-            ),
-          ),
+    return Consumer(
+      builder: (context, ref, child) {
+        final activeBranch = ref.watch(activeBranchProvider);
+        return activeBranch.when(
+          data: (branch) {
+            return Padding(
+              padding: const EdgeInsets.only(right: 12.0),
+              child: SizedBox(
+                height: 40,
+                width: 40,
+                child: ProfileWidget(
+                  branch: branch,
+                  sessionActive: true,
+                  size: 25,
+                  showIcon: false,
+                ),
+              ),
+            );
+          },
+          loading: () {
+            return Padding(
+              padding: const EdgeInsets.only(right: 12.0),
+              child: SizedBox(
+                height: 40,
+                width: 40,
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey.shade300,
+                  radius: 20,
+                ),
+              ),
+            );
+          },
+          error: (error, stackTrace) => const SizedBox.shrink(),
         );
       },
     );
