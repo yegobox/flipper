7f8b063c0561e65cfb3449460a7b9a699b673078
Muragijimana <info@yegobox.com>
2025-07-26 17:53:49 +0200

    fix: ...

--- packages/flipper_dashboard/test/quick_sell_test.dart
+++ packages/flipper_dashboard/test/quick_sell_test.dart
@@ -3,13 +3,16 @@ import 'package:flipper_models/db_model_export.dart';
 import 'package:flipper_models/providers/active_branch_provider.dart';
 import 'package:flipper_models/providers/transaction_items_provider.dart';
 import 'package:flipper_models/providers/transactions_provider.dart';
-import 'package:flipper_rw/dependency_initializer.dart';
+import 'package:flipper_models/providers/access_provider.dart';
 import 'package:flipper_services/proxy.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:supabase_models/brick/repository/storage.dart';
+
+import 'test_helpers/mocks.dart';
+import 'test_helpers/setup.dart';
 
 // flutter test test/quick_sell_test.dart  --dart-define=FLUTTER_TEST_ENV=true
 // Mocks
@@ -22,8 +25,6 @@ class MockTransactionItem extends Mock implements TransactionItem {}
 class MockBranch extends Mock implements Branch {}
 
 class MockCustomer extends Mock implements Customer {}
-
-class MockCoreViewModel extends Mock implements CoreViewModel {}
 
 class TestApp extends StatelessWidget {
   final Widget child;
@@ -50,6 +51,9 @@ class TestApp extends StatelessWidget {
         transactionItemsStreamProvider(transactionId: "test_transaction_id")
             .overrideWith((ref) => Stream.value(mockTransactionItems)),
         activeBranchProvider.overrideWith((ref) => Stream.value(mockBranch)),
+        featureAccessProvider(userId: 0, featureName: "Tickets")
+            .overrideWith((ref) => true),
+        isAdminProvider(0, featureName: "Tickets").overrideWith((ref) => true),
       ],
       child: MaterialApp(
         home: Scaffold(body: child),
@@ -69,21 +73,26 @@ void main() {
   late TextEditingController paymentTypeController;
   late TextEditingController countryCodeController;
   late GlobalKey<FormState> formKey;
+  late TestEnvironment env;
+  late MockDatabaseSync mockDbSync;
+  //  late CoreViewModel coreViewModel;
 
   setUpAll(() async {
-    await initializeDependenciesForTest();
+    env = TestEnvironment();
+    await env.init();
     registerFallbackValue(MockITransaction());
     registerFallbackValue(MockTransactionItem());
     registerFallbackValue(MockBranch());
     registerFallbackValue(MockCustomer());
-    registerFallbackValue(MockCoreViewModel());
+    // coreViewModel = CoreViewModel();
   });
 
   setUp(() {
+    env.injectMocks();
+    mockDbSync = env.mockDbSync;
     mockBoxService = MockBoxService();
     mockTransaction = MockITransaction();
     mockBranch = MockBranch();
-
     // Initialize controllers first
     discountController = TextEditingController();
     deliveryNoteController = TextEditingController();
@@ -109,18 +118,30 @@ void main() {
         key: any(named: 'key'),
         value: any(named: 'value'))).thenAnswer((_) async {});
     when(() => mockBoxService.paymentMethodCode(any())).thenReturn("01");
+    when(() => mockDbSync.isBranchEnableForPayment(
+            currentBranchId: any(named: 'currentBranchId')))
+        .thenAnswer((_) async => true);
+    when(() => mockBoxService.getBranchId()).thenReturn(1);
+    when(() => mockBoxService.getBusinessId()).thenReturn(1);
+    when(() => mockBoxService.customerTin()).thenReturn(null);
+    when(() => mockBoxService.writeString(
+        key: any(named: 'key'),
+        value: any(named: 'value'))).thenAnswer((_) async {});
+    when(() => mockBoxService.writeDouble(
+        key: any(named: 'key'),
+        value: any(named: 'value'))).thenAnswer((_) async {});
+    when(() => mockBoxService.writeBool(
+        key: any(named: 'key'),
+        value: any(named: 'value'))).thenAnswer((_) async {});
+    when(() => mockBoxService.paymentMethodCode(any())).thenReturn("01");
+    when(() => mockDbSync.isBranchEnableForPayment(
+            currentBranchId: any(named: 'currentBranchId')))
+        .thenAnswer((_) async => true);
 
     ProxyService.box = mockBoxService;
   });
 
-  tearDown(() {
-    discountController.dispose();
-    deliveryNoteController.dispose();
-    receivedAmountController.dispose();
-    customerPhoneNumberController.dispose();
-    paymentTypeController.dispose();
-    countryCodeController.dispose();
-  });
+  tearDown(() {});
 
   group('QuickSellingView Tests', () {
     testWidgets('displays correctly on small device', (tester) async {
@@ -188,6 +209,12 @@ void main() {
       when(() => mockItem.name).thenReturn("Test Item");
       when(() => mockItem.price).thenReturn(100.0);
       when(() => mockItem.qty).thenReturn(1.0);
+      when(() => mockDbSync.updateTransactionItem(
+            transactionItemId: any(named: 'transactionItemId'),
+            ignoreForReport: any(named: 'ignoreForReport'),
+            qty: any(named: 'qty'),
+            active: any(named: 'active'),
+          )).thenAnswer((_) async => 100.0);
 
       await tester.pumpWidget(
         MediaQuery.fromView(
@@ -210,7 +237,7 @@ void main() {
         ),
       );
 
-      await tester.pumpAndSettle();
+      // await tester.pumpAndSettle();
 
       // Tap the add quantity button
       await tester.tap(find.byKey(const Key('quantity-add-1')));
@@ -221,6 +248,7 @@ void main() {
             transactionItemId: "1",
             ignoreForReport: false,
             qty: 2.0,
+            active: any(named: 'active'),
           )).called(1);
     });
 
@@ -240,6 +268,11 @@ void main() {
       when(() => mockItem.name).thenReturn("Test Item");
       when(() => mockItem.price).thenReturn(100.0);
       when(() => mockItem.qty).thenReturn(1.0);
+      when(() => mockDbSync.updateTransactionItem(
+          transactionItemId: any(named: 'transactionItemId'),
+          ignoreForReport: any(named: 'ignoreForReport'),
+          qty: any(named: 'qty'),
+          active: any(named: 'active'))).thenAnswer((_) async => true);
 
       await tester.pumpWidget(
         MediaQuery.fromView(
