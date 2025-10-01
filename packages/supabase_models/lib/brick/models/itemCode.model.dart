a931873ef72fc3b790e9567401a7b5fd83753a74
Muragijimana <info@yegobox.com>
2025-10-01 22:44:27 +0200

    feat: generators for generic data adapdation

--- packages/supabase_models/lib/brick/models/itemCode.model.dart
+++ packages/supabase_models/lib/brick/models/itemCode.model.dart
@@ -1,11 +1,22 @@
+import 'package:brick_ditto_generators/ditto_sync_adapter.dart';
 import 'package:brick_offline_first_with_supabase/brick_offline_first_with_supabase.dart';
 import 'package:brick_sqlite/brick_sqlite.dart';
 import 'package:brick_supabase/brick_supabase.dart';
 import 'package:uuid/uuid.dart';
+import 'package:brick_offline_first/brick_offline_first.dart';
+import 'package:flipper_services/proxy.dart';
+import 'package:flutter/foundation.dart' show debugPrint, kDebugMode;
+import 'package:supabase_models/sync/ditto_sync_adapter.dart';
+import 'package:supabase_models/sync/ditto_sync_coordinator.dart';
+import 'package:supabase_models/sync/ditto_sync_generated.dart';
+import 'package:supabase_models/brick/repository.dart';
+
+part 'itemCode.model.ditto_sync_adapter.g.dart';
 
 @ConnectOfflineFirstWithSupabase(
   supabaseConfig: SupabaseSerializable(tableName: 'codes'),
 )
+@DittoAdapter('codes')
 class ItemCode extends OfflineFirstWithSupabaseModel {
   @Supabase(unique: true)
   @Sqlite(index: true, unique: true)
