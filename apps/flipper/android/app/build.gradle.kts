e7b3b102c1bb01bb128deb248138becca9760e6d
Muragijimana <info@yegobox.com>
2025-11-18 14:17:18 +0200

    fix(android): resolve inconsistent jvm target compatibility issue

--- apps/flipper/android/app/build.gradle.kts
+++ apps/flipper/android/app/build.gradle.kts
@@ -21,13 +21,13 @@ android {
     ndkVersion = "28.2.13676358"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
     }
 
     sourceSets["main"].java.srcDirs("src/main/kotlin")
--- apps/flipper/android/build.gradle.kts
+++ apps/flipper/android/build.gradle.kts
@@ -1,10 +1,12 @@
 buildscript {
+    val kotlin_version = "1.9.0"
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.5.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
 }
 
